62c1560a7f837a3874dda0aec23a09fbd8562a7a
Nghiem Duy <nghiem.eo.bua.18@gmail.com>
2023-07-07 00:57:41 +0700

    temporarily delete doctor seed for convenience

--- server/src/ignore/5_seeder-doctors.js
+++ server/src/ignore/5_seeder-doctors.js
@@ -1,15 +1,18 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('Doctors', [
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert("Doctors", [
       //đã khai báo ở seeder logins
     ]);
-  }, 
+  },
 
-  async down (queryInterface, Sequelize) {
-    return queryInterface.bulkDelete('User', null, { truncate: true, cascade: true })
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("User", null, {
+      truncate: true,
+      cascade: true,
+    });
 
     /**
      * Add commands to revert seed here.
@@ -17,5 +20,5 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+  },
 };
