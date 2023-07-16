93fc151e45138f856cb6da9da3568c8a4d143c84
Nghiem Duy <nghiem.eo.bua.18@gmail.com>
2023-07-16 22:30:55 +0700

    add clinic and fix about us

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import background from '../asset/background2-1.jpg';
+import background from '../asset/background2-2.jpg';
 import { useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 
@@ -31,7 +31,7 @@ export const Landing = () => {
         <img
           src={background}
           alt="Landing Page"
-          className="object-cover h-max"
+          className="object-cover w-screen h-fit object-center"
         />
         <div className="absolute  top-40 left-1/4 w-2/4 ">
           <h1 className=" font-bold text-white text-3xl">#1 Health Care</h1>
@@ -48,7 +48,7 @@ export const Landing = () => {
             <button onClick={() => navigate('/about-us')}>Đọc thêm</button>
           </div>
         </div>
-        <div className=" top-1/3 absolute bg-white opacity-90">
+        <div className=" top-2/3 absolute bg-white opacity-90">
           <div className="py-10 mx-40 inline-block w-fit">
             <h1 className=" text-gray-700">This is Hong Phuong</h1>
             <h1 className="  font-light text-5xl">Welcome to our Clinic</h1>
@@ -68,10 +68,6 @@ export const Landing = () => {
               will ensure your peace of mind and self-love.
             </p>
           </div>
-        </div>
-        <div className="absolute top-2/3 w-screen h-screen bg-white p-20 ">
-          {' '}
-          <p></p>
         </div>
       </div>
     </>
